56a35d5dd362144e3cee8ff519ab03e51b93512b
Rouven Harmgarth <rouven@harmgarth.de>
2018-05-08 11:03:07 +0200

    just for the look and feel

--- Blatt 2/Aufgabe2_5.c
+++ Blatt 2/Aufgabe2_5.c
@@ -8,28 +8,30 @@ int main()
   int y = 0;
   //Input mat1
   printf("Matrix 1: \n");
-  for (int y = 0; y < 3; y++)
+  for (y = 0; y < 3; y++)
   {
-    for (int x = 0; x < 3; x++)
+    for (x = 0; x < 3; x++)
     {
+	  printf("(%d,%d)", y, x);
       scanf_s("%d", &mat1[x][y]);
     }
   }
   //Input mat2
   printf("\nMatrix 2: \n");
-  for (int y = 0; y < 3; y++)
+  for (y = 0; y < 3; y++)
   {
-    for (int x = 0; x < 3; x++)
+    for (x = 0; x < 3; x++)
     {
+	  printf("(%d,%d)", y, x);
       scanf_s("%d", &mat2[x][y]);
     }
   }
   //Output
-  for (int y = 0; y < 3; y++)
+  for (y = 0; y < 3; y++)
   {
-    for (int x = 0; x < 3; x++)
+    for (x = 0; x < 3; x++)
     {
-      printf("%d     ", mat1[x][y] + mat2[x][y]);
+      printf("%d; ", mat1[x][y] + mat2[x][y]);
     }
     printf("\n");
   }
